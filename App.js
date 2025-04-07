68d6f6f3ef880d4fa7559ba19565c665262b0488
Ike <73871151+ike-byte@users.noreply.github.com>
2025-04-07 23:46:25 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './HomeScreen';
+import DrillScreen from './DrillScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Drill" component={DrillScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
